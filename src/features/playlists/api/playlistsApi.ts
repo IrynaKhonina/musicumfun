604cb7041c96eb51a3aa7af527c63ab17cbe3b89
IrynaKhonina <irinahonina@yahoo.com>
2025-08-30 11:43:21 +0300

    reate playlistsapi

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -1,6 +1,6 @@
 
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-
+import type {FetchPlaylistsArgs, PlaylistsResponse} from "@/features/playlists/api/playlistsApi.types.ts";
 export const playlistsApi = createApi({
 
     reducerPath: 'playlistsApi',
