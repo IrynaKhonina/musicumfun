fbdc0ff04590b27856ef6a033e32227f38ade516
IrynaKhonina <irinahonina@yahoo.com>
2025-09-22 21:04:06 +0300

    PlaylistCover: toast

--- src/features/playlists/ui/PlaylistItem/PlaylistCover.tsx
+++ src/features/playlists/ui/PlaylistItem/PlaylistCover.tsx
@@ -3,6 +3,7 @@ import {useDeletePlaylistCoverMutation, useUploadPlaylistCoverMutation} from "@/
 import type {ChangeEvent} from "react";
 import defaultCover from '@/assets/images/default-playlist-cover.png'
 import s from './PlaylistDescription/ PlaylistCover.module.css'
+import {toast} from "react-toastify";
 
 type Props = {
     playlistId: string
@@ -24,12 +25,16 @@ export const PlaylistCover = ({images, playlistId}: Props) => {
         if (!file) return
 
         if (!allowedTypes.includes(file.type)) {
-            alert('Only JPEG, PNG or GIF images are allowed')
+            toast('Only JPEG, PNG or GIF images are allowed', {type: 'error', theme: 'colored'})
         }
 
         if (file.size > maxSize) {
-            alert(`The file is too large. Max size is ${Math.round(maxSize / 1024)} KB`)
+            toast(`The file is too large (max. ${Math.round(maxSize / 1024)} KB)`, {
+                type: 'error',
+                theme: 'colored',
+            })
         }
+
 
         uploadCover({playlistId, file})
     }
