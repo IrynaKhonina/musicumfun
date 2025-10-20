361665e1952e35fbc0815a647bed7748b737bf58
IrynaKhonina <irinahonina@yahoo.com>
2025-10-20 11:07:33 +0300

    useGlobalLoading hook

--- src/common/hooks/useGlobalLoading.ts
+++ src/common/hooks/useGlobalLoading.ts
@@ -1,6 +1,14 @@
 import type { RootState } from '@/app/model/store.ts'
 import { useSelector } from 'react-redux'
+import {tracksApi} from "@/features/tracks/api/tracksApi.ts";
+import {playlistsApi} from "@/features/playlists/api/playlistsApi.ts";
 
+
+
+const excludedEndpoints = [
+    playlistsApi.endpoints.fetchPlaylists.name,
+    tracksApi.endpoints.fetchTracks.name,
+]
 export const useGlobalLoading = () => {
     return useSelector((state: RootState) => {
         // Получаем все активные запросы из RTK Query API
@@ -8,7 +16,14 @@ export const useGlobalLoading = () => {
         const mutations = Object.values(state.baseApi.mutations || {})
 
         // Проверяем, есть ли активные запросы (статус 'pending')
-        const hasActiveQueries = queries.some(query => query?.status === 'pending')
+        const hasActiveQueries = queries.some(query => {
+            if (query?.status !== 'pending') return
+            if (excludedEndpoints.includes(query.endpointName)) {
+                const completedQueries = queries.filter(q => q?.status === 'fulfilled')
+                return completedQueries.length > 0
+            }
+        })
+
         const hasActiveMutations = mutations.some(mutation => mutation?.status === 'pending')
 
         return hasActiveQueries || hasActiveMutations
